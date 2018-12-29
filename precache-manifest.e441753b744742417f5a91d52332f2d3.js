53974891cc9c4dbaccc0042faec86c917ff5ce3d
abhay-srivastav <abhay@Administrators-MacBook-Air-4.local>
2018-12-29 19:39:09 +0530

    Updates

---
+++ precache-manifest.e441753b744742417f5a91d52332f2d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Abhay07/movieAppReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc8eb821802379806137",
+    "url": "/Abhay07/movieAppReact/static/js/runtime~main.bc8eb821.js"
+  },
+  {
+    "revision": "8f4199c3148408630da2",
+    "url": "/Abhay07/movieAppReact/static/js/main.8f4199c3.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/Abhay07/movieAppReact/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "8f4199c3148408630da2",
+    "url": "/Abhay07/movieAppReact/static/css/main.117d9c9e.chunk.css"
+  },
+  {
+    "revision": "a29d40cdd6be263a5a51162b2d0925bc",
+    "url": "/Abhay07/movieAppReact/index.html"
+  }
+];
