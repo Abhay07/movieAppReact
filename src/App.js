564663e6d04e55c841c5f29d7f29f3a3c2965aaa
abhay-srivastav <abhay@Administrators-MacBook-Air-4.local>
2018-12-29 19:31:10 +0530

    Simple movie website in React for comparison

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MovieList from './MovieList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="header">Movies of 2018 (React)</header>
+        <MovieList />
       </div>
     );
   }
---
+++ src/MovieList.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './MovieList.css';
+
+class MovieList extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			movies:[],
+			titleOrder:'asc',
+			yearOrder:'asc'
+		}
+		this.sort = this.sort.bind(this);
+	}
+
+
+	sort(para){
+		const { movies } = this.state;
+		let { titleOrder, yearOrder } = this.state;
+		if(para == 'name'){
+			movies.sort((a,b)=>{
+				if(titleOrder == 'asc'){
+					return a.Title.charCodeAt(0) - b.Title.charCodeAt(0)
+				}
+
+				return b.Title.charCodeAt(0) - a.Title.charCodeAt(0)
+			})
+			titleOrder = titleOrder == 'asc' ? 'dsc' : 'asc';
+		}
+		else{
+			movies.sort((a,b)=>{
+				if(yearOrder == 'asc'){
+					return a.Year - b.Year
+				}
+
+				return b.Year - a.Year;
+			})
+			yearOrder = yearOrder == 'asc' ? 'dsc' : 'asc';
+		}
+
+
+		this.setState({
+			movies,
+			titleOrder,
+			yearOrder
+		});
+	}
+
+	componentDidMount(){
+	   fetch("http://www.omdbapi.com/?apikey=a567cf6c&s=love&type=movie")
+      .then(res => res.json())
+      .then((res)=>{
+      	this.setState({movies:res.Search});
+      })
+      .catch()
+	}
+	render(){
+		const { movies } = this.state;
+		return ( <div>
+				<div className="flex table-header">
+					<div>Poster</div>
+					<div onClick={()=>this.sort('name')}>
+						Title 
+						<span className="arrow-up"></span>
+						<span className="arrow-down"></span>
+					</div>
+					<div onClick={()=>this.sort('year')}>
+						Year
+						<span className="arrow-up"></span>
+						<span className="arrow-down"></span>
+					</div>
+				</div>
+				{
+					movies.map((mov,i)=>{
+						return (
+							<div className="flex row" key={i}>
+								<div>
+									<img src={mov.Poster} className="poster"/>
+								</div>
+								<div>{mov.Title}</div>
+								<div>{mov.Year}</div>
+							</div>
+						)
+					})
+				}
+			</div>
+		)
+	}
+}
+
+export default MovieList;
